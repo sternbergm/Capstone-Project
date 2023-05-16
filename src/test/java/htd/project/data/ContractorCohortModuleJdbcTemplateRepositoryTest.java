b1b4ded34f4f622daa840210aec20247e3918ca0
sternbergm <mishasternberg@gmail.com>
2023-05-16 10:55:35 -0400

    All tests written for ContractorCohortModule.

--- src/test/java/htd/project/data/ContractorCohortModuleJdbcTemplateRepositoryTest.java
+++ src/test/java/htd/project/data/ContractorCohortModuleJdbcTemplateRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContractorCohortModuleJdbcTemplateRepositoryTest {
@@ -23,29 +26,75 @@ class ContractorCohortModuleJdbcTemplateRepositoryTest {
 
     @Test
     void findAll() {
+        List<ContractorCohortModule> result = repository.readAll();
+
+        assertEquals(4, result.size());
     }
 
     @Test
     void findByContractor() {
+        List<ContractorCohortModule> result = repository.readByContractor(1);
+
+        assertEquals(2, result.size());
+
+        result = repository.readByContractor(500);
+
+        assertEquals(0, result.size());
     }
 
     @Test
     void findByCohort() {
+        List<ContractorCohortModule> result = repository.readByCohort(1);
+
+        assertEquals(2, result.size());
+
+        result = repository.readByCohort(500);
+
+        assertEquals(0, result.size());
     }
 
     @Test
     void findByModule() {
+        List<ContractorCohortModule> result = repository.readByModule(1);
+
+        assertEquals(2, result.size());
+
+        result = repository.readByModule(500);
+
+        assertEquals(0, result.size());
     }
 
     @Test
     void create() {
+        ContractorCohortModule ccm = new ContractorCohortModule(1, 2, 1, BigDecimal.valueOf(75));
+
+        ContractorCohortModule result  = repository.create(ccm);
+
+        assertEquals(ccm, result);
+        assertEquals(5, repository.readAll().size());
     }
 
     @Test
     void update() {
+        ContractorCohortModule ccm = new ContractorCohortModule(1, 1, 1, BigDecimal.valueOf(75));
+
+        assertTrue(repository.update(ccm));
+
+        assertEquals(4, repository.readAll().size());
+
+        ccm.setCohortId(500);
+        assertFalse(repository.update(ccm));
     }
 
     @Test
     void deleteByIds() {
+
+        assertTrue(repository.deleteByIds(1,1,1));
+
+        assertEquals(3, repository.readAll().size());
+
+        assertFalse(repository.deleteByIds(300,1,1));
+        assertFalse(repository.deleteByIds(1,200,1));
+        assertFalse(repository.deleteByIds(1,1,200));
     }
 }
