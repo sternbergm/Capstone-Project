0fc316c78b86d36a2fa8e4a39b719a54eaf3f905
nnshirisha <naganandini.shirisha@gmail.com>
2023-05-18 15:46:10 -0400

    Instructor Service is done

--- frontend/controllers/instructorService.js
+++ frontend/controllers/instructorService.js
@@ -6,34 +6,34 @@ const prompt = createPrompt();
 export async function instructorController() {
     let keepRunning = true;
     let response;
-    console.log("1. Add Clients\n2. Read Client By id\n3.add client\n4. update client\n 5.delete client");
+    console.log("1. Add Instructors\n2. Read Instructor By id\n3.add Instructor\n4. update Instructor\n 5.delete Instructor");
     let choice = prompt("Enter choice: ");
         switch (choice) {
             case "1":
-                response=await getAllClients();
+                response=await getAllInstructors();
                 console.log(response);
                     break;
             case "2":
-                response = await getClientById(); 
+                response = await getInstructorById(); 
                 console.log(response);
                 break;
             case "3":
-                response = await addClient();
+                response = await addInstructor();
                 console.log(response);
                 break;
             case "4":
-                response=await updateclient();
+                response=await updateinstructor();
                 console.log(response);
                 break;
             case "5": 
-                response=await deleteClient();
+                response=await deleteinstructor();
                 console.log(response);
                 break;
     }
 
 }
-async function getAllClients() {
-    return fetch("http://localhost:8080/client", {method: "GET", headers: {"Content-Type": "application/json"}})
+async function getAllInstructors() {
+    return fetch("http://localhost:8080/instructor", {method: "GET", headers: {"Content-Type": "application/json"}})
     .then((response) => {
         if (response.status !== 200) {
             console.log(response);
@@ -43,9 +43,9 @@ async function getAllClients() {
     }).catch((err) => console.log(err));
 }
 
-async function getClientById() {
+async function getInstructorById() {
     let clientId = prompt("Enter client Id you wish to view");
-    return fetch(`http://localhost:8080/client/${clientId}`, {method: "GET", headers: {"Content-Type": "application/json"}})
+    return fetch(`http://localhost:8080/instructor/$instructorId}`, {method: "GET", headers: {"Content-Type": "application/json"}})
     .then((response) => {
         if (response.status !== 200) {
             console.log(response);
@@ -55,16 +55,17 @@ async function getClientById() {
     }).catch((err) => console.log(err));
 }
 
-async function addClient() {
+async function addInstructor() {
     let token = await getAuth();
     const data = {
-        "ClientName": prompt("Enter ClientName "),
-        "Address": prompt("Enter Client Address topic"),
-        "CompanySize": prompt("Enter Client CompanySize "),
-        "Email": prompt("Enter Client Email ")
+        "firstName": prompt("Enter Instructor FirstName "),
+        "lastName": prompt("Enter Instructor LastName "),
+        "yearsOfExperience":("Enter Instructor Years of Experience"),
+        "expertise": prompt("Enter Instructor FirstName "),
+        "salary": prompt("Enter Instructor salary ")
     }
 
-    return fetch("http://localhost:8080/client", 
+    return fetch("http://localhost:8080/instructor", 
     {method: "POST", 
     headers: {"Content-Type": "application/json", accept: "application/json", authorization: "Bearer " + token.jwt_token}, 
     body: JSON.stringify(data)})
@@ -77,19 +78,20 @@ async function addClient() {
     });
 }
 
-async function updateclient() {
+async function updateinstructor() {
     let token = await getAuth();
-    let clientId = prompt("what is the Id of the client you wish to update?");
+    let instructorId = prompt("what is the Id of the instructor you wish to update?");
     const data = {
-        "clientId": clientId,
-        "ClientName": prompt("Enter ClientName "),
-        "Address": prompt("Enter Client Address topic"),
-        "CompanySize": prompt("Enter Client CompanySize "),
-        "Email": prompt("Enter Client Email ")
+        "instructorId": instructorId,
+        "firstName": prompt("Enter Instructor FirstName "),
+        "lastName": prompt("Enter Instructor LastName "),
+        "yearsOfExperience":("Enter Instructor Years of Experience"),
+        "expertise": prompt("Enter Instructor FirstName "),
+        "salary": prompt("Enter Instructor salary ")
+        
     }
     
-
-    return fetch(`http://localhost:8080/client/${clientId}`, 
+    return fetch(`http://localhost:8080/instructor/${instructorId}`, 
     {method: "PUT", 
     headers: {"Content-Type": "application/json", accept: "application/json", authorization: "Bearer " + token.jwt_token}, 
     body: JSON.stringify(data)})
@@ -100,5 +102,21 @@ async function updateclient() {
         }
         return response.json();
     });
-
 }
+
+    async function deleteinstructor() {
+        let token = await getAuth();
+        let instructorId = prompt("what is the Id of the instructor you wish to delete? ");
+    
+        return fetch(`http://localhost:8080/module/${instructorId}`, 
+        {method: "DELETE", 
+        headers: {"Content-Type": "application/json", accept: "application/json", authorization: "Bearer " + token.jwt_token}})
+        .then((response) => {
+            if (response.status !== 204) {
+                console.log(response);
+                return Promise.reject("The promise was not okay.");
+            }
+            return `Instructor ${instructorId} was deleted.`;
+        });
+    }
+
